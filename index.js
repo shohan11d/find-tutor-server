eec12d00a307c12c869bfa3caae2e797d4d4b397
shohan11d <shohan11d@gmail.com>
2026-05-21 16:37:56 +0600

    cors

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: true, credentials: true }));
-app.options("*", cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
